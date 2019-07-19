f58f90974e41297cb3ba5f3b414e57a68a426b15
Rahul Dhiman <rsdm34@gmail.com>
2019-07-19 10:47:11 +0000

    Db.js changes

--- db.js
+++ db.js
@@ -1,9 +1,40 @@
-const todos = [
-    {
-        id: 1,
-        title: "lunch",
-        description: "Go for lunc by 2pm"
-    }
-];
+// Get the mysql service
+var mysql = require('mysql');
 
-export default todos;
+// Add the credentials to access your database
+var connection = mysql.createConnection({
+    host      : 'localhost',
+    user      : 'root',
+    password  : 'R@hul1234',
+    database  : 'TestingDB',
+    debug     : true
+});
+
+// connect to mysql
+connection.connect(function (err) {
+    // in case of error
+    if (err) {
+        console.log("Connection Error")
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+    
+});
+
+// Perform a query
+$query = 'SELECT * FROM `Users`';
+
+connection.query($query, function (err, rows, fields) {
+    if (err) {
+        console.log("An error ocurred performing the query.");
+        return;
+    }
+
+    console.log("Query succesfully executed: ", rows);
+});
+
+// Close the connection
+connection.end(function () {
+    // The connection has been closed
+});
+

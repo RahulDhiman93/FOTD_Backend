52a2d5cf00906cd325192bd6c5848ca0ca902dba
RahulDhiman93 <rsdm34@gmail.com>
2019-07-22 15:26:28 +0530

    Now in app.js

--- app.js
+++ app.js
@@ -1,16 +1,73 @@
-import express from 'express';
-import db from './db';
-// Set up the express app
-const app = express();
-// get all todos
-app.get('/api/v1/todos', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'todos retrieved successfully',
-        todos: db
-    })
-});
+// Get the mysql service
+var express = require('express');
+const bodyParser = require('body-parser');
+var mysql = require('mysql');
 const PORT = 3000;
+var userData;
+
+// Add the credentials to access your database
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'R@hul1234',
+    database: 'TestingDB'
+});
+
+// connect to mysql
+connection.connect(function (err) {
+    // in case of error
+    if (err) {
+        console.log("Connection Error")
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+// get all todos
+app.get('/getUsers', (req, res) => {
+
+    var query = 'SELECT * FROM `Users`';
+
+    connection.query(query, function (err, rows, fields) {
+        if (err) {
+            console.log(err, "An error ocurred performing the query.");
+            return;
+        }
+        res.status(200).send({
+            success: 'true',
+            message: 'Data retrieved successfully',
+            data: rows
+        })
+        console.log("Query succesfully executed: ", rows);
+    });
+});
+
+app.post('/addUser', (req, res) => {
+
+    console.log(req.body);
+    var query = 'INSERT INTO `Users`(`user_name`) VALUES (?)';
+
+    connection.query(query, [req.body.user_name], function (err, rows, fields) {
+        if (err) {
+            console.log(err, "An error ocurred performing the query.");
+            return;
+        }
+        res.status(200).send({
+            success: 'true',
+            message: 'User added successfully',
+            data: req.body
+        })
+        console.log("Query succesfully executed: ", rows);
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)

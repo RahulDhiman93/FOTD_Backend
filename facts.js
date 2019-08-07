1197585891cad5b10cfe5d072e06db78686f926b
RahulDhiman93 <rsdm34@gmail.com>
2019-08-07 11:18:46 +0530

    bug fix

--- facts.js
+++ facts.js
@@ -327,7 +327,7 @@ function checkAppVersion (req, res) {
 
     console.log('APP VERSION API HITTED');
     let query = 'SELECT * FROM `FactAppVersion`';
-    let app_version = req.query.app_version;
+    let app_version = parseInt(req.query.app_version);
     connection.query(query, (err, rows, fields) => {
         if (err) {
             console.log(err);
@@ -338,8 +338,10 @@ function checkAppVersion (req, res) {
             });
         }
 
-        console.log('Current app version :',rows[0].app_version);
-        if (app_version != rows[0].app_version) {
+        let server_app_version = parseInt(rows[0].app_version);
+        console.log('Current app version :',server_app_version);
+
+        if (app_version < server_app_version) {
             return res.status(200).send({
                 status: false,
                 message: 'Please update your app to the latest version',

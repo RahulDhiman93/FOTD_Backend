22fede6d3d3f0aad2dbf44efb797d8727f776e44
RahulDhiman93 <rsdm34@gmail.com>
2023-06-27 11:58:28 -0700

    Logs for daily blog update added

--- modules/notification/service/notificationService.js
+++ modules/notification/service/notificationService.js
@@ -277,6 +277,7 @@ function scheduleNotification() {
 function scheduleTodayFactInBlog() {
 	console.log("scheduling today fact to blog")
 	schedule.scheduleJob("0 23 * * *", function () {
+		console.error("Updating blog section");
 		sendDailyFactToBlog()
 	});
 }
